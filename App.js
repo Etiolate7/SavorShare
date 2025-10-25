babc57de49929020e7d50af3c849645b3d12cb50
Marie <marie.roche32@orange.fr>
2025-10-25 22:59:34 +0200

    [ADD] profilescreen, fontawesome, css

--- App.js
+++ App.js
@@ -3,31 +3,52 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome5 } from '@expo/vector-icons';
+
 import HomeScreen from './screens/HomeScreen';
 import CreateScreen from './screens/CreateScreen';
 import RecipesScreen from './screens/RecipesScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
- return (
-   <Tab.Navigator screenOptions={{ headerShown: false }}>
-     <Tab.Screen name="Recipes" component={RecipesScreen} />
-     <Tab.Screen name="Create" component={CreateScreen} />
-   </Tab.Navigator>
- );
+  return (
+    <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size }) => {
+        let iconName = '';
+
+        if (route.name === 'Recipes') {
+          iconName = 'hamburger';
+        } else if (route.name === 'Create') {
+          iconName = 'plus';
+        } else if (route.name === 'Profile') {
+          iconName = 'user-alt';
+        }
+
+        return <FontAwesome5 name={iconName} size={20} color={color} />;
+      },
+      tabBarActiveTintColor: '#F03737',
+      tabBarInactiveTintColor: '#574040',
+      headerShown: false,
+    })}>
+      <Tab.Screen name="Recipes" component={RecipesScreen} />
+      <Tab.Screen name="Create" component={CreateScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
 }
 
 export default function App() {
   return (
-   <NavigationContainer>
-     <Stack.Navigator screenOptions={{ headerShown: false }}>
-       <Stack.Screen name="Home" component={HomeScreen} />
-       <Stack.Screen name="TabNavigator" component={TabNavigator} />
-     </Stack.Navigator>
-   </NavigationContainer>
- );
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="TabNavigator" component={TabNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,43 @@
-import { Button, StyleSheet, Text, View, TouchableOpacity, Image, ImageBackground } from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity, Image, ImageBackground, TextInput } from 'react-native';
 
 export default function HomeScreen({ navigation }) {
- return (
-   <View style={styles.container}>
-     <Text>Home Screen</Text>
-     <Button
-       title="Go to App"
-       onPress={() => navigation.navigate('TabNavigator')}
-     />
-   </View>
- );
+    return (
+        <View style={styles.container}>
+            <Text>Home Screen</Text>
+            <TextInput style={styles.input} placeholder='Username'></TextInput>
+            <TextInput style={styles.input} placeholder='Email'></TextInput>
+            <TextInput style={styles.input} placeholder='Password'></TextInput>
+            <TouchableOpacity style={styles.btn} activeOpacity={0.8}>
+                <Button
+                    style={styles.btnText}
+                    title="Go to App"
+                    color='white'
+                    onPress={() => navigation.navigate('TabNavigator')}
+                />
+            </TouchableOpacity>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    input: {
+        width: '80%',
+        marginTop: 25,
+        borderBottomColor: 'grey',
+        borderBottomWidth: 1,
+        fontSize: 16,
+    },
+    btn: {
+        alignItems: 'center',
+        width: '30%',
+        backgroundColor: '#F03737',
+        borderRadius: 10,
+        marginTop: 20,
+    }
 });
